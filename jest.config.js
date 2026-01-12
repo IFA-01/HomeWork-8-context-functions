c00a1e0cb5af8a561174a8db0ae71c856d5d51ce
IFA-01 <bogatyr.oleg@yahoo.com>
2026-01-12 20:21:11 +0300

    coverageThreshold added

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,12 @@ export default {
   testMatch: ['**/src/**/*.test.js'],
   collectCoverageFrom: ['src/**/*.js', '!src/**/*.test.js'],
   setupFilesAfterEnv: [],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
 };
